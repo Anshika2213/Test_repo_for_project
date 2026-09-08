d1b75e7be959fa3aa6596453326527376eaf87c3
Anshika2213 <anshika2213@gmail.com>
2026-09-08 12:34:20 +0530

    changed-3

--- calc.py
+++ calc.py
@@ -1,12 +1,8 @@
 def calculate_average(numbers):
     """Returns the average of a list of numbers."""
-    if not numbers:
-        return 0.0
     return sum(numbers) / len(numbers)
 
 
 def divide(a, b):
     """Returns a divided by b."""
-    if b == 0:
-        return 0.0
     return a / b
